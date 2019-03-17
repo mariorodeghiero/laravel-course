7a502f1d3961e697c15cf6c671caed264dc11cc4
Mário Antônio do Amaral Rodeghiero <mario.rodeghiero@gmail.com>
2019-03-17 11:52:05 -0300

    add: routes

--- projeto1/routes/web.php
+++ projeto1/routes/web.php
@@ -26,3 +26,19 @@ Route::get('/test/{nome}/{sobrenome}', function ($nome, $sobrenome) {
 Route::post('/add/{token}', function ($token) {
     return "<H1>Post: $token</H1>";
 });
+
+Route::delete('/rest/hello', function () {
+    return "<H1>Delete </H1>";
+});
+
+Route::put('/rest/hello', function () {
+    return "<H1>Put </H1>";
+});
+
+Route::patch('/rest/hello', function () {
+    return "<H1>Patch </H1>";
+});
+
+Route::options('/rest/hello', function () {
+    return "<H1>Option </H1>";
+});
